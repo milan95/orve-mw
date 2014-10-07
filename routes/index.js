19eba5dd15ae7acc19a95db23a778cf6176f1095
Milan Savani <milansavani@Milans-MacBook-Pro.local>
2014-10-07 02:11:30 -0400

    database setup

--- routes/index.js
+++ routes/index.js
@@ -5,27 +5,173 @@ var fs = require('fs');
 
 var ACCESS_TOKEN = 'ubrRcUkNB93YS5y7UByZ4WjWAq7HzuGj'
 
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Orve Home' });
-});
-
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Orve Home' });
 });
 
-/* GET Hello World page. */
-router.get('/control', function(req, res) {
+/* GET Userlist page. */
+router.get('/userlist', function(req, res) {
+    var db = req.db;
+    var collection = db.get('usercollection');
+    collection.find({},{},function(e,docs){
+        res.render('userlist', {
+            "userlist" : docs
+        });
+    });
+});
+
+/* GET admin page */
+router.get('/admin', function(req, res) {
     // DO DATABASE CHECK FOR SHOW ENABLED/DISABLED
-    res.render('control', { title: 'Ticketing Control Panel',
-                            disabled10158: false,
+    res.render('admin', { title: 'Administrator Control Panel',
+                          tix1015800: 10,
+                          tix1016730: 20,
+                          tix1016900: 30,
+                          tix1017730: 40,
+                          tix1017900: 50,
+                          tix1018730: 60,
+                          tix1018900: 70,
+                          sold1015800: 1,
+                          sold1016730: 2,
+                          sold1016900: 3,
+                          sold1017730: 4,
+                          sold1017900: 5,
+                          sold1018730: 6,
+                          sold1018900: 7,
+                          disabled1015800: false,
+                          disabled1016730: false,
+                          disabled1016900: false,
+                          disabled1017730: false,
+                          disabled1017900: false,
+                          disabled1018730: false,
+                          disabled1018900: false });
+    
+});
+
+/* GET seller page page. */
+router.get('/seller', function(req, res) {
+    // DO DATABASE CHECK FOR SHOW ENABLED/DISABLED
+    res.render('seller', { title: 'Ticketing Control Panel',
+                            disabled1015800: false,
                             disabled1016730: false,
-                            disabled10169: false,
+                            disabled1016900: false,
                             disabled1017730: false,
-                            disabled10179: false,
+                            disabled1017900: false,
                             disabled1018730: false,
-                            disabled10189: false });
-    db.usercollection.drop();
+                            disabled1018900: false });
+});
+
+/* POST to Update Shows Service */
+router.post('/updateshows', function(req, res) {
+  var db = req.db;
+
+  var show1015800 = req.body.disabled1015800;
+  var show1016730 = req.body.disabled1016730;
+  var show1016900 = req.body.disabled1016900;
+  var show1017730 = req.body.disabled1017730;
+  var show1017900 = req.body.disabled1017900;
+  var show1018730 = req.body.disabled1018730;
+  var show1018900 = req.body.disabled1018900;
+
+  var avail1015800 = req.body.num1015800;
+  var avail1016730 = req.body.num1016730;
+  var avail1016900 = req.body.num1016900;
+  var avail1017730 = req.body.num1017730;
+  var avail1017900 = req.body.num1017900;
+  var avail1018730 = req.body.num1018730;
+  var avail1018900 = req.body.num1018900;
+
+  db.shows.update(
+    { date: "10_15-800PM" },
+    {
+      $set: {
+        acceptingTickets: show1015800,
+        tixAvail: avail1015800
+      }
+    },
+    {
+      upsert: true
+    }
+  );
+
+  db.shows.update(
+    { date: "10_16-730PM" },
+    {
+      $set: {
+        acceptingTickets: show1016730,
+        tixAvail: avail1016730
+      }
+    },
+    {
+      upsert: true
+    }
+  );
+
+  db.shows.update(
+    { date: "10_16-900PM" },
+    {
+      $set: {
+        acceptingTickets: show1016900,
+        tixAvail: avail1016900
+      }
+    },
+    {
+      upsert: true
+    }
+  );
+
+  db.shows.update(
+    { date: "10_17-730PM" },
+    {
+      $set: {
+        acceptingTickets: show1017730,
+        tixAvail: avail101730
+      }
+    },
+    {
+      upsert: true
+    }
+  );
+
+  db.shows.update(
+    { date: "10_17-900PM" },
+    {
+      $set: {
+        acceptingTickets: show1017900,
+        tixAvail: avail1017900
+      }
+    },
+    {
+      upsert: true
+    }
+  );
+
+  db.shows.update(
+    { date: "10_18-730PM" },
+    {
+      $set: {
+        acceptingTickets: show1018730,
+        tixAvail: avail1018730
+      }
+    },
+    {
+      upsert: true
+    }
+  );
+
+  db.shows.update(
+    { date: "10_18-900PM" },
+    {
+      $set: {
+        acceptingTickets: show1018900,
+        tixAvail: avail1018900
+      }
+    },
+    {
+      upsert: true
+    }
+  );
 });
 
 /* POST to Charge Customer Service */
@@ -46,12 +192,10 @@ router.post('/chargecustomer', function(req, res) {
     var message = "Mask%20and%20Wig%20" + userShow + "Show";
     var formResponse = "Venmo Charge for " + userName + " at phone number " + userPhone + " for show " + userShow + " for $" + userAmt + " was successful.";
 
-    res.render('control', { title: 'Ticketing Control Panel', formResult: formResponse });
+    res.render('seller', { title: 'Ticketing Control Panel', formResult: formResponse });
 
     // Set our collection
     var collection = db.get('usercollection');
-
-
 
     //var TXN = PostChargeRequest(ACCESS_TOKEN, userPhone, newAmt, message);
 
@@ -69,7 +213,7 @@ router.post('/chargecustomer', function(req, res) {
             //res.send("There was a problem processing the charge request.");
         }
         else {
-            //document.getElementById("responseMessage").innerHTML = "New text!";
+            // All worked. Do something?
         }
     });
 
