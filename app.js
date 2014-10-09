e6e7a022b6469ec4f46c8c287ebcecc0560d0f55
Milan Savani <milansavani@Milans-MacBook-Pro.local>
2014-10-09 15:12:02 -0400

    heroku mongo

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongo = require('mongodb');
 var monk = require('monk');
-var db = monk('localhost:27017/orve-mw');
+// var db = monk('localhost:27017/orve-mw'); // local
+var db = monk('heroku_app30474414:g46b84jut300slvdkh9u86e1pp@ds043180.mongolab.com:43180/heroku_app30474414'); // heroku
+
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
